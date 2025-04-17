c1d548aa2d80d645c3cb05dda669a542bf68b430
Kagol <kagol@sina.com>
2025-04-17 16:03:15 +0800

    feat: add opentiny-icons

---
+++ src/components/DemoIcons.vue
@@ -0,0 +1,13 @@
+<template>
+  <i class="ci-home" style="font-size:14px; color:#000;"></i>
+  <i class="ci-email" style="font-size:16px; color:#d32222;"></i>
+  <i class="ci-news" style="font-size:18x; color:#4822d3;"></i>
+  <i class="ci-date" style="font-size:20px; color:#40d322;"></i>
+  <hr />
+  <i class="ci-svc-ecs" style="font-size:72px;"></i>
+  <i class="ci-svc-obs" style="font-size:72px;"></i>
+  <i class="ci-svc-cbr" style="font-size:72px;"></i>
+  <i class="ci-svc-live" style="font-size:72px;"></i>
+  <hr />
+  <i class="myicon-like" style="font-size:40px;"></i>
+</template>
