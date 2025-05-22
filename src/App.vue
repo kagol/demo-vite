c298e6208702dc30096e8cfc9476453d02220df8
Kagol <kagol@sina.com>
2025-05-22 14:53:44 +0800

    feat: 用户基于 mcp-tiny-vue 提供的工具，扩展自己的业务工具

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <script setup lang="ts">
 import { ref, onMounted, getCurrentInstance } from 'vue'
-import { TinyBaseSelect, TinyButton } from '@opentiny/vue'
+import { TinyBaseSelect, TinyButton, TinySwitch } from '@opentiny/vue'
 import { Client } from '@modelcontextprotocol/sdk/client/index.js'
 import { createTransportPair } from '@opentiny/next'
+import { z } from 'zod'
 
 // 创建一对可互通的 Transport 实例
 const [serverTransport, clientTransport] = createTransportPair()
+const { mcpServerTinyVue: server, globalState } = getCurrentInstance().appContext.config.globalProperties
 
 let client
 
 onMounted(async () => {
-  const { mcpServerTinyVue: server, globalState } = getCurrentInstance().appContext.config.globalProperties
-
   client = new Client({
     name: 'example-client',
     version: '1.0.0'
+  })
+
+  // 用户自定义的 MCP 工具
+  server.tool('switchNFC', '开启或关闭 NFC 功能', { value: z.boolean() }, async ({ value }) => {
+    console.log('server tool switchNFC', value)
+    switchValue.value = value
+
+    // 用户自定义的 MCP 工具调用 TinyVue 的 MCP 工具
+    await client.callTool({
+      name: 'selectOption',
+      arguments: {
+        value: 'shanghai'
+      }
+    })
+
+    return { content: [{ type: 'text', text: String(switchValue.value) }] }
   })
 
   // 建立服务端和客户端的通信连接
@@ -23,22 +39,43 @@ onMounted(async () => {
 })
 
 const options = ref([
-  { value: '选项 1', label: '北京' },
-  { value: '选项 2', label: '上海' },
-  { value: '选项 3', label: '天津' },
-  { value: '选项 4', label: '重庆' },
-  { value: '选项 5', label: '深圳' }
+  { value: 'beijing', label: '北京' },
+  { value: 'shanghai', label: '上海' },
+  { value: 'tianjin', label: '天津' },
+  { value: 'chongqing', label: '重庆' },
+  { value: 'shenzhen', label: '深圳' }
+])
+
+const options2 = ref([
+  { value: 'changfen', label: '肠粉' },
+  { value: 'huoguo', label: '牛肉火锅' },
+  { value: 'yeziji', label: '椰子鸡' },
+  { value: 'zhujiaofan', label: '猪脚饭' },
+  { value: 'guotiao', label: '牛肉粿条' }
 ])
 
 const autoHandleSelect = async () => {
   const result = await client.callTool({
     name: 'selectOption',
     arguments: {
-      value: '选项 2'
+      value: 'shanghai'
     }
   })
 
   console.log('result====', result)
+}
+
+const switchValue = ref(false)
+
+const autoHandleSwitchNFC = async () => {
+  const result = await client.callTool({
+    name: 'switchNFC',
+    arguments: {
+      value: switchValue.value ? false : true
+    }
+  })
+
+  console.log('NFC result====', result)
 }
 </script>
 
@@ -53,7 +90,16 @@ const autoHandleSelect = async () => {
   </div>
 
   <div style="display: flex; align-items: center;">
-    <tiny-base-select v-model="globalState.selectModelValue" :options="options"> </tiny-base-select>
-    <tiny-button type="primary" size="mini" round style="margin-left: 12px;" @click="autoHandleSelect">AI 自动操作</tiny-button>
+    <div>
+      下拉框1：<tiny-base-select ref="selectRef1" v-model="globalState.selectModelValue" :options="options"></tiny-base-select>
+      下拉框2：<tiny-base-select ref="selectRef2" v-model="globalState.selectModelValue2" :options="options2"></tiny-base-select>
+    </div>
+    <tiny-button type="primary" size="mini" round style="margin-left: 12px;" @click="autoHandleSelect">AI 自动操作【下拉框1】</tiny-button>
+    <tiny-button type="primary" size="mini" round style="margin-left: 12px;" @click="autoHandleSelect">AI 自动操作【下拉框2】</tiny-button>
+  </div>
+
+  <div>
+    <tiny-switch v-model="switchValue"></tiny-switch>
+    <tiny-button type="primary" size="mini" round style="margin-left: 12px;" @click="autoHandleSwitchNFC">AI 自动开启、关闭 NFC 功能</tiny-button>
   </div>
 </template>
